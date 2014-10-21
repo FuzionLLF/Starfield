810e854ea31efaa7904e56a1bbe5a2f0fcba8e14
fuzionllf <fuzion117@gmail.com>
2014-10-21 11:04:20 -0700

    chris.html

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle [] group;//your code here
-void setup()
+public void setup()
 {
   size(720,480);
   background(0);
@@ -10,7 +26,7 @@ void setup()
       group[i] = new NormalParticle();
     }
 }
-void draw()
+public void draw()
 {
   background(0);
   for(int i = 0;i < group.length;i++)
@@ -71,5 +87,14 @@ class OddballParticle implements Particle
     fill(myColor);
     noStroke();
     ellipse((float)x,(float)y,20,20);
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
