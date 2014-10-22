9dd3e75bbe53fe10c960974db2e004c14eda8503
fuzionllf <fuzion117@gmail.com>
2014-10-22 11:29:44 -0700

    particles.html

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -17,77 +17,88 @@ public class Starfield extends PApplet {
 Particle [] group;//your code here
 public void setup()
 {
-  size(720,480);
-  background(0);
-  group = new Particle[50];
-  group[49] = new OddballParticle();
-    for(int i = 0; i < 49;i++)
-    {
-      group[i] = new NormalParticle();
-    }
+	size(720,480);
+	background(0);
+	group = new Particle[51];
+	group[49] = new OddballParticle();
+	group[50] = new Jumbo();
+	for(int i = 0; i < 49;i++)
+	{
+		group[i] = new NormalParticle();
+	}
 }
 public void draw()
 {
-  background(0);
-  for(int i = 0;i < group.length;i++)
-  {
-    group[i].show();
-    group[i].move();
-
-  }
+	background(0);
+	for(int i = 0;i < group.length;i++)
+	{
+		group[i].show();
+		group[i].move();
+	}
   //your code here
 }
-  interface Particle{
-    public void show();
-    public void move();
-  }
+interface Particle
+{
+	public void show();
+	public void move();
+}
 class NormalParticle implements Particle
 {
-  int myColor;
-  double x,y,mySpeed,myAngle;
+	int myColor;
+	double x,y,mySpeed,myAngle;
   NormalParticle() //constructor
   {
-    x = 360;
-    y = 240;
-    mySpeed = Math.random()*5+2;
-    myAngle = Math.PI*2*Math.random();
-    myColor = 255;
+  	x = 360;
+  	y = 240;
+  	mySpeed = Math.random()*5+2;
+  	myAngle = Math.PI*2*Math.random();
+  	myColor = 255;
   }
   public void move()
   {
-    x = Math.cos(myAngle)*mySpeed + x;
-    y = Math.sin(myAngle)*mySpeed + y;
+  	x = Math.cos(myAngle)*mySpeed + x;
+  	y = Math.sin(myAngle)*mySpeed + y;
   }
   public void show()
   {
-    fill(myColor);
-    noStroke();
-    ellipse((float)x,(float)y,10,10);
+  	fill(myColor);
+  	noStroke();
+  	ellipse((float)x,(float)y,10,10);
   }
 } 
 class OddballParticle implements Particle
 {
-  int myColor;
-  double x,y,mySpeed,myAngle;
-  OddballParticle()
-  {
-    x = 360;
-    y = 240;
-    mySpeed = Math.random()*5+2;
-    myAngle = Math.PI*2*Math.random();
-    myColor = 255;
-  }
-  public void move()
-  {
-    x = 1 + x;
-    y = 1 + y;
-  }
-  public void show()
-  {
-    fill(myColor);
-    noStroke();
-    ellipse((float)x,(float)y,20,20);
-  }
+	//int myColor;
+	double x,y,mySpeed,myAngle;
+	OddballParticle()
+	{
+		x = 360;
+		y = 240;
+		mySpeed = Math.random()*5+2;
+		myAngle = Math.PI*2*Math.random();
+	}
+	public void move()
+	{
+		x =  Math.cos(myAngle)*mySpeed + x;
+		y =  Math.sin(myAngle)*mySpeed + y;
+	}
+	public void show()
+	{
+		fill(0,255,0);
+		noStroke();
+		ellipse((float)x,(float)y,50,50);
+	}
+}
+class Jumbo extends NormalParticle
+{
+	Jumbo()
+	{}
+	public void show()
+	{
+		fill(255,0,0);
+		ellipse((float)x,(float)y,100,100);
+	}
+	
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Starfield" };
